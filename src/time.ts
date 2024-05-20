0e36191721e8346edcef7b01fa709e1febfa52bb
sergio <ser.alvarez1998@gmail.com>
2024-05-19 23:53:18 -0400

    no ofset

--- src/time.ts
+++ src/time.ts
@@ -1,6 +1,6 @@
 export function get_datetime_chile(): Date {
     var date = new Date();
-    var offset = -date.getTimezoneOffset();
+    var offset = 0 // -date.getTimezoneOffset();
     var chile_date = new Date(date.getTime() + offset * 60 * 1000);
     return chile_date;
 }
